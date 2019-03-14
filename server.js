332c9771cbe2cdc600fa734f7503770c3ad6ed81
Rafael Jesus <rafaeljesus.martins@gmail.com>
2019-03-13 22:10:22 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const http = require('http');
+const app = require('../src/app');
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('listening', () => console.log(`Server está rodando na porta: ${port}`));
+server.on('error', (error) => console.log(`Ocorreu um erro: ${error}`));
+
+function normalizePort(val) {
+    const port = parseInt(val, 10)
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
